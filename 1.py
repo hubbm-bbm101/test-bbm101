22c571e1fc07062939a1e3a35cd193c082a4666b
bbm101 <32355052+bbm101@users.noreply.github.com>
2020-10-07 16:33:00 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,18 +1,23 @@
 import sys
-from math import sqrt
-a=int(sys.argv[1])
-b=int(sys.argv[2])
-c=int(sys.argv[3])
-delta = b**2 - 4*a*c
-if(delta >0):
-    print("There are two solutions")
-    result1 = (-b + sqrt(delta)) / (2*a)
-    result2 = (-b - sqrt(delta)) / (2*a)
-    print('Solutions: ' +  str(result1) + ' ' +  str(result2))
-elif(delta == 0):
-    print("There is only one solution")
-    result = -(b/2*a)
-    print("Solution: " + str(result))
-else:
-    print("There is no real solution")
 
+# quadratic equation solver
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+
+discr = b ** 2 - 4 * a * c
+
+if discr > 0:
+    print("Equation has a real solution")
+    print("There are two solutions")
+    root1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+    root2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+    print("Solution(s): {} {} ".format(round(root1,2), round(root2,2)))
+elif discr == 0:
+    print("Equation has a real solution")
+    print("There are one(two repeated) solution")
+    root = -b / (2 * a)
+    print("Solution(s): {} ".format(round(root,2)))
+else:
+    print("Neither of the solutions are real numbers")
